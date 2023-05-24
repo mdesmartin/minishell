2c02d1446d700399984fde15393ae8bcc25bfa5d
Julien Moutoussamy <jmoutous@z2r2p8.42lyon.fr>
2023-05-24 12:18:02 +0200

    FIX: ft_del_onelst correctly del the first lst

--- sources/builtin/unset.c
+++ sources/builtin/unset.c
@@ -6,29 +6,32 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 16:12:06 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/05/18 14:20:02 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/05/24 12:12:36 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	ft_del_onelst(t_envp *envp, char *variable)
+void	ft_del_onelst(t_envp **envp, char *variable)
 {
 	t_envp	*current;
 	t_envp	*previous;
 
-	current = envp;
-	previous = envp;
+	current = *envp;
+	previous = *envp;
 	while (current)
 	{
 		if (ft_strncmp(current->variable, variable, ft_strlen(variable)) == 0
 			&& ft_strlen(current->variable) == ft_strlen(variable))
 		{
-			previous->next = current->next;
+			if (current == *envp)
+				*envp = (*envp)->next;
+			else
+				previous->next = current->next;
 			free(current->variable);
 			free(current->value);
 			free(current);
-			break ;
+			return ;
 		}
 		else
 		{
@@ -36,6 +39,30 @@ void	ft_del_onelst(t_envp *envp, char *variable)
 			current = current->next;
 		}
 	}
+}
+
+static int	ft_check_unsetvar(char *variable)
+{
+	int	i;
+
+	i = 1;
+	if (ft_isalpha(variable[0]) == 0 && variable[0] != '_')
+	{
+		ft_dprintf(2, "minishell : unset: `%s': not a valid identifier\n",
+			variable);
+		return (1);
+	}
+	while (variable[i])
+	{
+		if (ft_isalnum(variable[i]) == 0 && variable[i] != '_')
+		{
+			ft_dprintf(2, "minishell : unset: `%s': not a valid identifier\n",
+				variable);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
 }
 
 void	ft_unset(t_data *data, char **command)
@@ -56,7 +83,7 @@ void	ft_unset(t_data *data, char **command)
 		if (ft_check_unsetvar(command[i]) == 1)
 			data->exit_code = 1;
 		else if (ft_strncmp("_", command[i], 2) != 0)
-			ft_del_onelst(data->envp, command[i]);
+			ft_del_onelst(&data->envp, command[i]);
 		i++;
 	}
 	ft_update_envptab(data);
