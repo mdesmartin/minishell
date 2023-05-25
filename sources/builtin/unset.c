0b9883df25508e712f609e54e7a320fafed1adae
jmoutous <jmoutous@student.42lyon.fr>
2023-05-25 18:21:47 +0200

    FIX: unset act like in bash

--- sources/builtin/unset.c
+++ sources/builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 16:12:06 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/05/24 12:12:36 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/05/25 17:58:27 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,30 +41,6 @@ void	ft_del_onelst(t_envp **envp, char *variable)
 	}
 }
 
-static int	ft_check_unsetvar(char *variable)
-{
-	int	i;
-
-	i = 1;
-	if (ft_isalpha(variable[0]) == 0 && variable[0] != '_')
-	{
-		ft_dprintf(2, "minishell : unset: `%s': not a valid identifier\n",
-			variable);
-		return (1);
-	}
-	while (variable[i])
-	{
-		if (ft_isalnum(variable[i]) == 0 && variable[i] != '_')
-		{
-			ft_dprintf(2, "minishell : unset: `%s': not a valid identifier\n",
-				variable);
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-
 void	ft_unset(t_data *data, char **command)
 {
 	int	i;
@@ -80,9 +56,7 @@ void	ft_unset(t_data *data, char **command)
 	}
 	while (command[i])
 	{
-		if (ft_check_unsetvar(command[i]) == 1)
-			data->exit_code = 1;
-		else if (ft_strncmp("_", command[i], 2) != 0)
+		if (ft_strncmp("_", command[i], 2) != 0)
 			ft_del_onelst(&data->envp, command[i]);
 		i++;
 	}
