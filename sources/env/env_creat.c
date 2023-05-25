e0b554635e4185256bb3bd25596e1ea7faa8cf17
Julien Moutoussamy <jmoutous@z1r3p2.42lyon.fr>
2023-05-25 15:17:03 +0200

    FIX: free pwd in env_creat.c

--- sources/env/env_creat.c
+++ sources/env/env_creat.c
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 15:25:51 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/05/24 10:38:22 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/05/25 15:14:25 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ void	ft_creat_envp(t_data *data)
 	if (!pwd)
 		ft_error(data, "Memory allocation failed: envp_creat", 12);
 	ft_creat_variable(data, "PWD", pwd);
+	free(pwd);
 	ft_creat_variable(data, "OLDPWD", NULL);
 	ft_creat_variable(data, "SHLVL", "1");
 }
