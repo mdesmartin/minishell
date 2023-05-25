231b838050c3d7a2fbb80919a8557fb5d1b8f1eb
jmoutous <jmoutous@student.42lyon.fr>
2023-05-25 18:23:19 +0200

    FIX: implement of cd ~/

--- sources/builtin/cd.c
+++ sources/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 12:47:41 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/05/18 15:21:58 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/05/25 18:19:05 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,8 @@ void	ft_builtin_cd(t_data *data, char **command)
 	ft_check_cd(data, command);
 	if (data->exit_code != 0)
 		return ;
-	if (command[1] == NULL || ft_strncmp("~", command[1], 2) == 0)
+	if (command[1] == NULL || ft_strncmp("~", command[1], 2) == 0
+		|| ft_strncmp("~/", command[1], 3) == 0)
 		ft_cd_home(data);
 	else if (ft_strncmp("-", command[1], 2) == 0)
 		ft_cd_back(data);
