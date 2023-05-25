f13c16f8cbdb1a1687c9fb7b9caa7f2d231a8a14
jmoutous <jmoutous@student.42lyon.fr>
2023-05-25 16:14:36 +0200

    FIX: fix here_doc

--- sources/process/here_doc_utils.c
+++ sources/process/here_doc_utils.c
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 14:00:26 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/05/25 14:30:27 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/05/25 16:10:04 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,25 +25,27 @@ void	ft_check_hd_expand(t_pipeline *pipe, char *limiter)
 		pipe->here_doc_expand = 0;
 }
 
-void	ft_error_heredoc(t_data *data, int *here_doc_fd, char *limiter)
+int	ft_error_heredoc(t_data *data, int *here_doc_fd, char *limiter)
 {
 	ft_putstr_fd("minishell: warning: here-document limiter wanted `", 2);
 	ft_putstr_fd(limiter, 2);
 	ft_putstr_fd("'\n", 2);
 	ft_close(here_doc_fd[0]);
 	ft_close(here_doc_fd[1]);
-	ft_quit(data, 1);
+	data->exit_code = 1;
+	return (1);
 }
 //message when ctrl+d in stead of ctrl+c
 
-void	ft_stop_heredoc(t_data *data, int *here_doc_fd, char *input)
+int	ft_stop_heredoc(t_data *data, int *here_doc_fd, char *input)
 {
 	g_exitcode--;
 	ft_close(here_doc_fd[0]);
 	ft_close(here_doc_fd[1]);
 	if (input)
 		free(input);
-	ft_quit(data, 2);
+	data->exit_code = 2;
+	return (1);
 }
 
 char	*ft_here_doc_expand(t_data *data, t_pipeline *pipe, char *input)
